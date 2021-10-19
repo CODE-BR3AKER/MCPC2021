b7db1537235324b9c2a795d1c66eb748f12fa249
CODE-BR3AKER <zarktizakaria@gmail.com>
2021-10-19 01:53:54 -0400

    contest added

--- practice/Problem C/main.c
+++ practice/Problem C/main.c
@@ -1,5 +1,19 @@
-#include <stdio.h>
+// Calculate the number of possible paths in the grid
+// add both the food and the kittens to a 2d array that you pass to the function as arguments
+// If the button array is empty then we only need to exclude paths that contains cells of kittens
+// if the button array is not empty then we need also to exclude paths that does not contain at least one button and to
 
+/*
+Logic of the function:
+    Mark the current cell as visited
+    If the current cell is a valid and open cell
+        go down (x, y) ——> (x + 1, y)
+        go right (x, y) ——> (x, y + 1)
+    backtrack from the current cell and remove it from the current path
+    return count
+*/
+
+/*
 int main()
 {
     FILE *file = fopen("mission.in", "r");
@@ -10,19 +24,21 @@ int main()
         int N, M, K, L;
         fscanf(file, "%d %d %d %d", &N, &M, &K, &L);
         printf("%d %d %d %d\n", N, M, K, L);
-        int kittens_x, kittens_y;
-        int food_x, food_y;
+        int kittens[K][2];
+        int btns[L][2];
         for (int x = 0; x < K; x++)
         {
-            fscanf(file, "%d %d", &kittens_x, &kittens_y);
-            printf("kittens:%d %d\n", kittens_x, kittens_y);
+
+            fscanf(file, "%d %d", &kittens[x][0], &kittens[x][1]);
+            printf("Kittens:%d %d\n", kittens[x][0], kittens[x][1]);
         }
         for (int x = 0; x < L; x++)
         {
-            fscanf(file, "%d %d", &food_x, &food_y);
-            printf("food:%d %d\n", food_x, food_y);
+            fscanf(file, "%d %d", &btns[x][0], &btns[x][1]);
+            printf("Buttons:%d %d\n", btns[x][0], btns[x][1]);
         }
+        printf("%d\n", countPaths(N, M, btns, kittens));
     }
     fclose(file);
     return 0;
-}
+}*/
